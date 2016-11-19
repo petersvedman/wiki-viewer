4bd38f27eb9499941256f8c0b7d8256093e4b749
Peter Svedman <peter.svedman@gmail.com>
2016-11-19 19:30:11 +0100

    Error fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Global variables here
+// global functions here
+
+// $(document).ready(function(){ do JQuery stuff here });
+
+// Shorthand logger for debugging
+function logit(obj){ console.log(obj);}
+
+
 var baseURL = "https://en.wikipedia.org//w/api.php?action=query&format=json&generator=search&gsrsearch=";
 var query = '';
 var randomURL = "https://en.wikipedia.org/wiki/Special:Random";
